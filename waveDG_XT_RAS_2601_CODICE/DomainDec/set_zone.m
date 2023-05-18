2aff72dbc7686fa27f3a20df3ee3798f3106e6a5
filippolimi <56077704+filippolimi@users.noreply.github.com>
2023-05-18 18:07:59 +0200

    Delete waveDG_XT_RAS_2601_CODICE directory

--- waveDG_XT_RAS_2601_CODICE/DomainDec/set_zone.m
+++
@@ -1,8 +0,0 @@
- % set subdomains inside the subdomain window (zone)
-function sub_zone = set_zone(subx,subt,sx,dx)
-
-matrix_sub = reshape(1:subx*subt,subt,subx);
-zonematrix= matrix_sub(sx:(dx-1),:);
-sub_zone = reshape(zonematrix,1,[]);
-
-end
--- waveDG_XT_RAS_2601_CODICE/waveDG_XT_RAS_2601_CODICE/MultiGrid/blockalize.m
+++
@@ -1,8 +0,0 @@
-function [Aj,bj] = blockalize(A,b)
-    
-    n = size(A,1);
-    J = createJ(n);
-    Aj = J*A*(J');
-    bj = J*b;
-    
-end
